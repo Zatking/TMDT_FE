966080c3f338a5631f4e2b077c6268838c641a63
Ngô Thanh Tâm <22dh114722@st.huflit.edu.vn>
2025-03-10 21:02:12 +0700

    10/03/2025 21:02

--- src/categories/psu.jsx
+++ src/categories/psu.jsx
@@ -6,35 +6,35 @@ export function PsuByQuality() {
       <li>
         <Link
           to="/ProductPage/PSU"
-          className="font-bold text-xl text-[#ff0000] mb-3 px-3">
+          className="font-bold text-xl text-[#ff0000] mb-3 px-3 hover:bg-transparent py-0">
           PSU
         </Link>
       </li>
       <li>
         <Link
-          to="/ProductPage/PSU/Bronze"
-          className="cursor-pointer px-3 hover:text-[#ff0000] border-x border-transparent hover:border-[#ff0000] rounded-lg">
+          to="/ProductPage/PSU//Bronze"
+          className="cursor-pointer px-3 hover:text-[#ff0000] border-x border-transparent hover:border-[#ff0000] rounded-lg hover:bg-transparent py-0">
           Bronze
         </Link>
       </li>
       <li>
         <Link
-          to="/ProductPage/PSU/Gold"
-          className="cursor-pointer px-3 hover:text-[#ff0000] border-x border-transparent hover:border-[#ff0000] rounded-lg">
+          to="/ProductPage/PSU//Gold"
+          className="cursor-pointer px-3 hover:text-[#ff0000] border-x border-transparent hover:border-[#ff0000] rounded-lg hover:bg-transparent py-0">
           Gold
         </Link>
       </li>
       <li>
         <Link
-          to="/ProductPage/PSU/Platium"
-          className="cursor-pointer px-3 hover:text-[#ff0000] border-x border-transparent hover:border-[#ff0000] rounded-lg">
+          to="/ProductPage/PSU//Platium"
+          className="cursor-pointer px-3 hover:text-[#ff0000] border-x border-transparent hover:border-[#ff0000] rounded-lg hover:bg-transparent py-0">
           Platium
         </Link>
       </li>
       <li>
         <Link
-          to="/ProductPage/PSU/Titanium"
-          className="cursor-pointer px-3 hover:text-[#ff0000] border-x border-transparent hover:border-[#ff0000] rounded-lg">
+          to="/ProductPage/PSU//Titanium"
+          className="cursor-pointer px-3 hover:text-[#ff0000] border-x border-transparent hover:border-[#ff0000] rounded-lg hover:bg-transparent py-0">
           Titanium
         </Link>
       </li>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -51,8 +51,12 @@ export default function Contact() {
                 <span className="ml-2">8:00 - 21:00 | Thứ 2 - Chủ Nhật</span>
               </p>
             </div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8665.607503865704!2d106.65337776692155!3d10.777796599502972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752edb765b5c25%3A0x9a3519bdad5a85a3!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBOZ2_huqFpIG5n4buvIC0gVGluIGjhu41jIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCAoSFVGTElUKQ!5e0!3m2!1svi!2s!4v1741581849559!5m2!1svi!2s"
+              className="w-full h-2/3"
+              loading="lazy"></iframe>
           </div>
-          <div className="p-4 bg-[#ff0000] text-[#fff] rounded-lg">
+          <div className="h-full p-4 bg-[#ff0000] text-[#fff] rounded-lg">
             <img
               src="https://lh3.googleusercontent.com/p/AF1QipMvsqL33YYJlYBJ_C3ZcLyXRosIYEFvaUAjBsyy=s1360-w1360-h1020"
               alt=""
@@ -125,6 +129,10 @@ export default function Contact() {
                 <span className="ml-2">8:00 - 21:00 | Thứ 2 - Chủ Nhật</span>
               </p>
             </div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15677.371612920404!2d106.64409529891257!3d10.785027400000025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fc0c12178fd%3A0x89de70633dc4df1d!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBOZ2_huqFpIG5n4buvIC0gVGluIGjhu41jIFRQLkhDTSBDxqEgc-G7nyBCYSBHaWEgKEhVRkxJVCBCYSBHaWEgQ2FtcHVzKQ!5e0!3m2!1svi!2s!4v1741582194154!5m2!1svi!2s"
+              className="w-full h-2/3"
+              loading="lazy"></iframe>
           </div>
           <div className="p-4 bg-[#ff0000] text-[#fff] rounded-lg">
             <img
