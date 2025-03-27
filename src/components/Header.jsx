c189db65ed0a4c848bd33d42cb8a7760a41e5b67
Ngô Thanh Tâm <22dh114722@st.huflit.edu.vn>
2025-03-27 09:34:24 +0700

    27/03/2025 09:34

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
   faCartShopping,
+  faStar,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faHeart as regularHeart,
@@ -17,10 +18,38 @@ import { useEffect, useState } from "react";
 const Header = () => {
   const [cartList, setCartList] = useState([]);
   const location = useLocation();
+  const [products, setProducts] = useState([]);
+  const [productSearch, setProductSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const apiUrl = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
     setCartList(localStorage.getItem("cartList", cartList));
   }, [cartList]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("https://node-tmdt.vercel.app/api/get-product");
+        const data = await res.json();
+        setProducts(data.products);
+      } catch (error) {
+        setError(error);
+        console.log("error: ", error);
+      }
+    };
+    fetchProducts();
+    setLoading(false);
+  }, []);
+
+  const toVND = (amount) => {
+    return new Intl.NumberFormat("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    }).format(amount);
+  };
 
   return (
     <div className="grid grid-cols-5 h-20 px-20 border border-b-black items-center static bg-[#fff]">
@@ -73,7 +102,9 @@ const Header = () => {
             } text-xl mt-1 hover:text-[#ff0000]`}
           />
         </Link>
-        <div className="flex">
+        <div
+          className="flex"
+          onClick={() => document.getElementById("my_modal_2").showModal()}>
           <input
             type="text"
             placeholder="What are you looking for?"
@@ -83,6 +114,46 @@ const Header = () => {
             <FontAwesomeIcon icon={faMagnifyingGlass} />
           </div>
         </div>
+        <dialog id="my_modal_2" className="modal">
+          <div className="modal-box">
+            <div className="flex w-full">
+              <input
+                type="text"
+                placeholder="What are you looking for?"
+                onChange={(e) => setProductSearch(e.target.value)}
+                className="px-2 bg-[#f5f5f5] h-10 w-full items-center rounded-l-lg outline-none"
+              />
+              <div className="w-20 h-10 flex justify-center items-center text-xl bg-[#f5f5f5] rounded-r-lg">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </div>
+            </div>
+            <div className="">
+              {productSearch ? (
+                products
+                  .filter((product) => product.ProName.includes(productSearch))
+                  .map((product) => (
+                    <Link
+                      to={"/productDetail/" + product._id}
+                      className="p-5 rounded-lg overflow-hidden shadow-lg flex h-20"
+                      key={product._id}>
+                      <img src={product.Images[0]} alt="" />
+                      <div className="">
+                        <p className="font-bold line-clamp-1">
+                          {product.ProName}
+                        </p>
+                        <p>{toVND(product.Price)}</p>
+                      </div>
+                    </Link>
+                  ))
+              ) : (
+                <p className="text-[#ff0000]">Type in something</p>
+              )}
+            </div>
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
         <FontAwesomeIcon
           id="heartIcon"
           icon={regularHeart}
